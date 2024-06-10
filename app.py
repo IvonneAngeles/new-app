8bca2981c3f089077e8508b9a002276fd22576e2
Ivonne <ivonneah99@gmail.com>
2024-06-10 14:56:28 -0600

    Update app.py
    quitamos un 0

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
->>>>>>> 0f00666f7446e88dbc0168e33ab544a2b36d6775
+>>>>>>> f00666f7446e88dbc0168e33ab544a2b36d6775
 st.write('Esta aplicación aún no es funcional. En construcción.')
