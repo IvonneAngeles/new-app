017dfd5c755189ab9be6e3ddf0e420dff7ae8284
Ivonne <ivonneah99@gmail.com>
2024-06-10 15:06:52 -0600

    Update app.py
    cambien el código

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.header('Lanzar una moneda')
 
-=======
-import streamlit as st
-
-st.header('Lanzar una moneda')
-
->>>>>>> f00666f7446e88dbc0168e33ab544a2b36d6775
 st.write('Esta aplicación aún no es funcional. En construcción.')
